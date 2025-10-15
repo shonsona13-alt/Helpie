e2954670152e1d109251a91e70c1b94fadc03144
shonsona13-alt <shonsona13@gmail.com>
2025-10-14 21:16:51 -0500

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,58 @@
+ 
+while True:
+    user_input: str = input('You: ')
+
+    if user_input == 'hello':
+        print ('Chatbot: Hello My name is Chatbot!')
+    elif user_input == 'Hello':
+        print ('Chatbot: Hello, My name is Chatbot!')
+    elif user_input == 'Hi':
+        print ('Chatbot: Hello, My name is Chatbot!')
+    elif user_input == 'hi':
+        print ('CHatbot: Hello, My name is Chatbot!')
+    elif user_input == 'How are you':
+        print('Chatbot: I am good, how about you?')
+    elif user_input == 'how are you':
+        print('Chatbot: I am good, How about you?')
+    elif user_input == 'How are you doing':
+        print('Chatbot: I am good, How about you?')
+    elif user_input == 'how are you doing':
+        print('Chatbot: I am good, How about you?')
+    elif user_input == 'How are you Chatbot':
+        print('Chatbot: I am good, How about you?')
+    elif user_input == 'how are you Chatbot':
+        print('Chatbot: I am good, How are you?')
+    elif user_input == 'What is your favorite food to eat':
+        print('Chatbot: I like to eat and digest binary numbers.')
+    elif user_input == 'Do pinapples belong on pizza':
+        print('Chatbot: Yes pinapples do belong on pizza')
+    elif user_input == 'Do you prefer waffles or pancakes':
+        print('Chatbot: I prefer pancakes over waffles because pancakes are softer than waffles')
+    elif user_input == 'I am good':
+        print('Chatbot: Nice')
+    elif user_input == 'I am well':
+        print('Chatbot: Nice' )
+    elif user_input == 'I am great':
+        print('Chatbot: Nice')
+    elif user_input == 'I am Sad':
+        print('Chatbot: Well, I hope you have a great day!')
+    elif user_input == 'I am not doing great':
+        print('Chatbot: Well, I hope you have a great day!')
+    elif user_input == 'I am mad':
+        print('Chatbot: well, I hope you have a great day!')
+    elif user_input == 'What is your favorite color':
+        print('Chatbot: My favorite color is Green')
+    elif user_input == 'What is your favorite thing to do':
+        print('Chatbot: My favorite thing to do is to talk to other people.')
+    elif user_input == 'What do you do in your free time':
+        print('Chatbot: I like to play video games such as Fortnite in my free time.')
+    elif user_input == 'Bye':
+        print('Chatbot: Goodbye!')
+    elif user_input == 'Goodbye':
+        print('Chatbot: Goodbye')
+    elif user_input == 'bye':
+        print('Chatbot: Goodbye')
+    elif user_input == 'goodbye':
+        print('Chatbot: Goodbye')
+    else:
+        print('Chatbot: I did not understand, pls try again or try something different, There may be a grammar mistake such as capitalization.')
