44ff7de2be80a3058761aff81b115e875d0178bd
shonsona13-alt <shonsona13@gmail.com>
2025-10-15 13:51:54 -0500

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,58 @@
+ 
+while True:
+    user_input: str = input('You: ')
+
+    if user_input == 'hello':
+        print ('Helpie: Hello My name is Helpie!')
+    elif user_input == 'Hello':
+        print ('Helpie: Hello, My name is Helpie!')
+    elif user_input == 'Hi':
+        print ('Helpie: Hello, My name is Helpie!')
+    elif user_input == 'hi':
+        print ('Helpie: Hello, My name is Helpie!')
+    elif user_input == 'How are you':
+        print('Helpie: I am good, how about you?')
+    elif user_input == 'how are you':
+        print('Helpie: I am good, How about you?')
+    elif user_input == 'How are you doing':
+        print('Helpie: I am good, How about you?')
+    elif user_input == 'how are you doing':
+        print('Helpie: I am good, How about you?')
+    elif user_input == 'How are you Helpie':
+        print('Helpie: I am good, How about you?')
+    elif user_input == 'how are you Helpie':
+        print('Helpie: I am good, How are you?')
+    elif user_input == 'What is your favorite food to eat':
+        print('Helpie: I like to eat and digest binary numbers.')
+    elif user_input == 'Do pinapples belong on pizza':
+        print('Helpie: Yes pinapples do belong on pizza')
+    elif user_input == 'Do you prefer waffles or pancakes':
+        print('Helpie: I prefer pancakes over waffles because pancakes are softer than waffles')
+    elif user_input == 'I am good':
+        print('Helpie: Nice')
+    elif user_input == 'I am well':
+        print('Helpie: Nice' )
+    elif user_input == 'I am great':
+        print('Helpie: Nice')
+    elif user_input == 'I am Sad':
+        print('Helpie: Well, I hope you have a great day!')
+    elif user_input == 'I am not doing great':
+        print('Helpie: Well, I hope you have a great day!')
+    elif user_input == 'I am mad':
+        print('Helpie: well, I hope you have a great day!')
+    elif user_input == 'What is your favorite color':
+        print('Helpie: My favorite color is Green')
+    elif user_input == 'What is your favorite thing to do':
+        print('Helpie: My favorite thing to do is to talk to other people.')
+    elif user_input == 'What do you do in your free time':
+        print('Helpie: I like to play video games such as Fortnite in my free time.')
+    elif user_input == 'Bye':
+        print('Helpie: Goodbye!')
+    elif user_input == 'Goodbye':
+        print('Helpie: Goodbye')
+    elif user_input == 'bye':
+        print('Helpie: Goodbye')
+    elif user_input == 'goodbye':
+        print('Helpie: Goodbye')
+    else:
+        print('Helpie: I did not understand, pls try again or try something different, There may be a grammar mistake such as capitalization.')
